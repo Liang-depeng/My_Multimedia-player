37ec07329b1fce2df236857b2675992c7e2b7a4b
Liang-depeng <18435737579@163.com>
2018-07-18 10:57:52 +0800

    MediaPlayer

--- app/src/main/java/ldp/example/com/mymultimediaplayer/Pager/VideoPager.java
+++ app/src/main/java/ldp/example/com/mymultimediaplayer/Pager/VideoPager.java
@@ -176,6 +176,7 @@ public class VideoPager extends BasePager {
             startActivity(intent);*/
 
 
+
         }
     }
 }
--- app/src/main/java/ldp/example/com/mymultimediaplayer/activity/SystemVideoPlayer.java
+++ app/src/main/java/ldp/example/com/mymultimediaplayer/activity/SystemVideoPlayer.java
@@ -19,6 +19,7 @@ public class SystemVideoPlayer extends Activity{
     private VideoView mVideoView;
     private Uri mUri;
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
